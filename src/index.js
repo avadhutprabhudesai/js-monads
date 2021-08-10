f9be9793ad74a0562721cbdf0832fc8e22a7ab86
avadhut <avadhutprabhudesai23@gmail.com>
2021-08-10 11:40:02 +0530

    feat: # Maybe monad
    Used real life example to demonstrate the use of Maybe monad. Maybe monad implementation is borrowed
    from monet.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-import './sass/main.scss';
+/* eslint-disable @babel/new-cap */
+import { MayBeMonad } from './maybe';
+
+MayBeMonad();
---
+++ src/mock-data.js
@@ -0,0 +1,44 @@
+export const users = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    address: {
+      country: 'IND',
+      city: 'Pune',
+    },
+  },
+  {
+    firstName: 'Paulo',
+    lastName: 'Martini',
+    address: {
+      country: 'ESP',
+    },
+  },
+  {
+    firstName: 'Smith',
+    lastName: 'Doe',
+  },
+  {
+    firstName: 'Marshall',
+    lastName: 'Doe',
+    address: {},
+  },
+  {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    address: {
+      country: 'SRB',
+    },
+  },
+  null,
+  undefined,
+];
+
+export const languageOfCountry = {
+  ind: 'Hindi',
+  usa: 'English',
+  esp: 'Spanish',
+  gbr: 'English',
+  ger: 'German',
+  rus: 'Russia',
+};
